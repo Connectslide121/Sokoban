f1c72dd656e2ee4862c5c2b6a2aaeec2a9cec1dd
Jon Mendizabal <jonmendi@gmail.com>
2023-12-05 14:57:47 +0100

    add player movement and score logic

--- script.js
+++ script.js
@@ -1,54 +1,160 @@
-//**** global variables */
+//***** global variables *****/
 
-let tileMap = tileMap01;
-let playerCoords;
-let win = false;
+let tileMap = tileMap01
+let tileType
+let playerCoords
+let totalGoalCount
+let goalCount
+let gameCompleted = false
 
-const map = document.getElementById("map");
-
-//**** create map */
-
-function CreateMap(){
-    for (let y = 0; y < tileMap.height ; y++) {
-        for (let x = 0; x < tileMap.width ; x++) {
-            const tile = document.createElement("div");
-            tile.className = "tile";
-            tile.id = "x" + x + "y" + y;
-    
-            let tileType = tileMap.mapGrid[y][x][0];
-    
-            if(tileType == "W"){
-                tile.classList.add(Tiles.Wall);
-            }
-    
-            else if(tileType == "P")
-            {
-                tile.classList.add(Entities.Character);
-                playerCoords = { x, y }
-            }
-    
-            else if(tileType == "B"){
-                tile.classList.add(Entities.Block);
-            }
-    
-            else if(tileType == "G"){
-                tile.classList.add(Tiles.Goal);
-            }
-    
-            else{
-                tile.classList.add(Tiles.Space)
-            }
-
-            map.appendChild(tile);
-        }
-    }    
+const tileClass = {
+    " ": Tiles.Space,
+    W: Tiles.Wall,
+    B: Entities.Block,
+    G: Tiles.Goal,
+    P: Entities.Character,
 }
 
-CreateMap()
+const map = document.getElementById("map")
+
+//***** create map *****/
+
+function CreateMap(){
+
+    totalGoalCount = 0
+    map.innerHTML = ""    
+
+    for (let y = 0; y < tileMap.height ; y++) {
+        for (let x = 0; x < tileMap.width ; x++) {
+            let tile = document.createElement("div")
+            tile.id = "x" + x + "y" + y; // Set tile ID according to its coords
+            tile.className = "tile"; // Set general tile class
+    
+            tileType = tileMap.mapGrid[y][x][0]
+            tile.classList.add(tileClass[tileType]) // Set class according to tile type
+    
+            if(tileType == "P") {playerCoords = { x, y }} // Set player coordinates
+            if(tileType == "G") {totalGoalCount ++} // Set total count of goals
+    
+            map.appendChild(tile)
+        }
+    }    
+
+    goalCount = totalGoalCount
+}
+
+CreateMap();
 
 
-//**** player movement */
+
+//***** player input *****/
+
+function playerInput(event){
+
+    if (event.key == ("ArrowUp") && gameCompleted != true) { movePlayer(0, -1); }
+    else if (event.key == ("w") && gameCompleted != true) { movePlayer(0, -1); }
+    else if (event.key == ("ArrowLeft") && gameCompleted != true) { movePlayer(-1, 0); }
+    else if (event.key == ("a") && gameCompleted != true) { movePlayer(-1, 0); }
+    else if (event.key == ("ArrowDown") && gameCompleted != true) { movePlayer(0, 1); }
+    else if (event.key == ("s") && gameCompleted != true) { movePlayer(0, 1); }
+    else if (event.key == ("ArrowRight") && gameCompleted != true) { movePlayer(1, 0); }
+    else if (event.key == ("d") && gameCompleted != true) { movePlayer(1, 0); }
+    event.preventDefault();
+}
+
+//***** player movement *****/
+
+function movePlayer(moveX, moveY){
+    
+    let moveIsValid = false
+
+    let targetTiles = [ document.getElementById("x" + playerCoords.x + "y" + playerCoords.y), // current player tile
+                        document.getElementById("x" + (playerCoords.x + moveX) + "y" + (playerCoords.y + moveY)), // player targer tile
+                        document.getElementById("x" + (playerCoords.x + 2 * moveX) + "y" + (playerCoords.y + 2 * moveY))] // box target tile
+
+    let targetTilesClasses = [targetTiles[0].classList, targetTiles[1].classList, targetTiles[2].classList]
 
 
+    if (targetTilesClasses[1].contains(Tiles.Space)) 
+    {
+        moveIsValid = true
+    }
 
-//**** global variables */
+    if (targetTilesClasses[1].contains(Tiles.Goal))
+    {
+        moveIsValid = true
+    }
+
+    if (targetTilesClasses[1].contains(Entities.Block) && targetTilesClasses[2].contains(Tiles.Space))
+    {
+        moveIsValid = true
+
+        targetTiles[2].classList.remove(Tiles.Space)
+        targetTiles[2].classList.add(Entities.Block)
+
+    }
+
+    if (targetTilesClasses[1].contains(Entities.Block) && targetTilesClasses[2].contains(Tiles.Goal))
+    {
+        moveIsValid = true
+
+        targetTiles[2].classList.remove(Tiles.Goal)
+        targetTiles[2].classList.add(Entities.BlockDone)
+
+        goalCount--
+    }
+
+    if (targetTilesClasses[1].contains(Entities.BlockDone) && targetTilesClasses[2].contains(Tiles.Goal))
+    {
+        moveIsValid = true
+
+        targetTiles[2].classList.remove(Tiles.Goal)
+        targetTiles[2].classList.add(Entities.BlockDone)
+    }
+
+    if (targetTilesClasses[1].contains(Entities.BlockDone) && targetTilesClasses[2].contains(Tiles.Space))
+    {
+        moveIsValid = true
+
+        targetTiles[2].classList.remove(Tiles.Space)
+        targetTiles[2].classList.add(Entities.Block)
+
+        goalCount++
+    }
+
+    if (moveIsValid == true)
+    {
+        if (tileMap.mapGrid[playerCoords.y][playerCoords.x][0] == "G")
+        {
+            targetTiles[0].classList.add(Tiles.Goal)
+        }
+        else{
+            targetTiles[0].classList.add(Tiles.Space)
+        }
+
+        playerCoords.x = playerCoords.x + moveX
+        playerCoords.y = playerCoords.y + moveY
+
+        targetTiles[0].classList.remove(Entities.Character)
+        targetTiles[1].classList= "tile"
+        targetTiles[1].classList.add(Entities.Character)
+    }
+
+    if (goalCount == 0) {gameComplete()}
+
+    console.log(targetTilesClasses)
+    console.log(moveIsValid)
+    console.log(goalCount)
+
+}
+
+function gameComplete(){
+
+    gameCompleted = true
+
+}
+
+
+//***** event listeners *****/
+
+document.addEventListener("keydown", playerInput);
